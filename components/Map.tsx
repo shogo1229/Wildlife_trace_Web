1c98879e9ea936a84454ca117aeb8c6e62c9fb10
shogo1229 <neko2000shogo@gmail.com>
2023-07-07 18:30:18 +0900

    全体MAP表示切替タブ実装

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,42 +1,110 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 interface MapProps {
-	latitude: number;
-	longitude: number;
+  latitude: number;
+  longitude: number;
 }
 
 const Map: React.FC<MapProps> = ({ latitude, longitude }) => {
-	const mapContainerRef = useRef<HTMLDivElement>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		// Load Google Maps API script
-		const googleMapsScript = document.createElement('script');
-		googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAIORL_apItyiqnoDPu4WRbnuBAmKV3WBM&libraries=places`;
-		googleMapsScript.async = true;
-		window.document.body.appendChild(googleMapsScript);
+  useEffect(() => {
+    // Load Google Maps API script
+    const googleMapsScript = document.createElement("script");
+    googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAIORL_apItyiqnoDPu4WRbnuBAmKV3WBM&libraries=places`;
+    googleMapsScript.async = true;
+    window.document.body.appendChild(googleMapsScript);
 
-	googleMapsScript.addEventListener('load', () => {
-	  // Initialize the map
-		const map = new google.maps.Map(mapContainerRef.current!, {
-			center: { lat: latitude, lng: longitude },
-			zoom: 15,
-					mapId:'6c6ec6ddb735a534'
-			});
+    googleMapsScript.addEventListener("load", () => {
+      // Initialize the map
+      const map = new google.maps.Map(mapContainerRef.current!, {
+        center: { lat: latitude, lng: longitude },
+        zoom: 15,
+        styles: [
+          {
+            stylers: [
+              {
+                saturation: -100,
+              },
+            ],
+          },
+          {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#0099dd",
+              },
+            ],
+          },
+          {
+            elementType: "labels",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#aadd55",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "labels.text",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "road.local",
+            elementType: "labels.text",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {},
+        ],
+      });
 
-	  // Place a pin at the provided coordinates
-		new google.maps.Marker({
-			position: { lat: latitude, lng: longitude },
-			map: map,
-		});
-	});
+      // Place a pin at the provided coordinates
+      new google.maps.Marker({
+        position: { lat: latitude, lng: longitude },
+        map: map,
+      });
 
-	return () => {
-	  // Clean up the Google Maps API script
-		window.document.body.removeChild(googleMapsScript);
-	};
-	}, [latitude, longitude]);
+      // Disable Street View control
+      map.setOptions({ streetViewControl: false });
 
-	return <div ref={mapContainerRef} style={{ height: '400px' }} />;
+      // Disable fullscreen control
+      map.setOptions({ fullscreenControl: false });
+    });
+
+    return () => {
+      // Clean up the Google Maps API script
+      window.document.body.removeChild(googleMapsScript);
+    };
+  }, [latitude, longitude]);
+
+  return <div ref={mapContainerRef} style={{ height: "400px" }} />;
 };
 
 export default Map;
--- components/multi_pin.tsx
+++ components/multi_pin.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
 import firebaseConfig from "../lib/firebase";
 import Image from "next/image";
@@ -21,7 +21,7 @@ const loadGoogleMapsAPI = () => {
     } else {
       const script = document.createElement("script");
       script.id = "google-maps-api";
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAIORL_apItyiqnoDPu4WRbnuBAmKV3WBM&libraries=places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places`;
       script.onload = resolve;
       script.onerror = reject;
       document.head.appendChild(script);
@@ -30,11 +30,15 @@ const loadGoogleMapsAPI = () => {
 };
 
 const MultiPIN_MAP = () => {
+  const [filter, setFilter] = useState<string>("all"); // Track the selected filter
   const [data, setData] = useState<Todo[]>([]);
   const [selectedPin, setSelectedPin] = useState<Todo | null>(null);
-  const [infoWindow, setInfoWindow] = useState<google.maps.InfoWindow | null>(
-    null
-  );
+  const infoWindowRef = useRef<google.maps.InfoWindow | null>(null);
+  const mapRef = useRef<google.maps.Map | null>(null);
+  const [selectedFilter, setSelectedFilter] = useState("all");
+  const handleFilterChange = (filter) => {
+    setSelectedFilter(filter);
+  };
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -65,55 +69,193 @@ const MultiPIN_MAP = () => {
     loadGoogleMapsAPI().then(() => {
       const map = new window.google.maps.Map(document.getElementById("map"), {
         zoom: 16,
+        disableDefaultUI: true, // Disable the default UI controls
+        styles: [
+          {
+            stylers: [
+              {
+                saturation: -100,
+              },
+            ],
+          },
+          {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#0099dd",
+              },
+            ],
+          },
+          {
+            elementType: "labels",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#aadd55",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "labels",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "labels.text",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "road.local",
+            elementType: "labels.text",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {},
+        ],
       });
 
+      mapRef.current = map;
+
       if (data.length > 0) {
-        // Get the last added pin's coordinates
         const lastPin = data[data.length - 1];
         const lastPinPosition = {
           lat: lastPin.latitude,
           lng: lastPin.longitude,
         };
 
-        // Set the map's center to the last pin's position
         map.setCenter(lastPinPosition);
 
-        // Display pins
         data.forEach((todo) => {
+          let iconImage;
+          if (todo.class === "Boar") {
+            iconImage = "/icons/Boar_pin_Red.png";
+          } else if (todo.class === "Deer") {
+            iconImage = "/icons/Deer_pin_Red.png";
+          }
+
           const marker = new window.google.maps.Marker({
             position: { lat: todo.latitude, lng: todo.longitude },
             map: map,
             title: todo.class,
+            icon: {
+              url: iconImage,
+              scaledSize: new window.google.maps.Size(32, 32),
+            },
           });
 
-          // Add click event listener to the marker
+          if (filter === "all" || todo.class === filter) {
+            marker.setVisible(true);
+          } else {
+            marker.setVisible(false);
+          }
+
           marker.addListener("click", () => {
             setSelectedPin(todo);
-            if (infoWindow) {
-              infoWindow.close();
+            if (infoWindowRef.current) {
+              infoWindowRef.current.close();
             }
             const newInfoWindow = new window.google.maps.InfoWindow({
               content: `
-                <div>
-                  <p>Latitude: ${todo.latitude}</p>
-                  <p>Longitude: ${todo.longitude}</p>
-                  <p>Class: ${todo.class}</p>
-                  <p>User: ${todo.user}</p>
-                  <p>path* ${todo.imagepath}</p>
-                  <img src="${todo.imagepath}" alt="Pin Image" width="250" height="250" />
-                </div>
+              <div class="max-w-xs p-4 flex">
+              <div class="mr-4">
+                <img src="${todo.imagepath}" alt="Pin Image" width="100%" style="max-width: 250px; height: auto;" />
+              </div>
+              <div>
+                <p class="mb-2">
+                  <strong class="text-blue-500 font-bold">Latitude:</strong>
+                  <br>
+                  <span class="font-bold">${todo.latitude}</span>
+                </p>
+                <p class="mb-2">
+                  <strong class="text-blue-500 font-bold">Longitude:</strong>
+                  <br>
+                  <span class="font-bold">${todo.longitude}</span>
+                </p>
+                <p class="mb-2">
+                  <strong class="text-blue-500 font-bold">Class:</strong>
+                  <br>
+                  <span class="font-bold">${todo.class}</span>
+                </p>
+                <p class="mb-2">
+                  <strong class="text-blue-500 font-bold">User:</strong>
+                  <br>
+                  <span class="font-bold">${todo.user}</span>
+                </p>
+              </div>
+            </div>
+            
               `,
             });
-            setInfoWindow(newInfoWindow);
+            infoWindowRef.current = newInfoWindow;
             newInfoWindow.open(map, marker);
+
+            // Close the InfoWindow when clicking outside of it
+            google.maps.event.addListener(map, "click", () => {
+              newInfoWindow.close();
+            });
           });
         });
       }
     });
-  }, [data]);
+  }, [data, filter]);
 
   return (
     <div>
+      <div className="border-b border-gray-200 dark:border-gray-700">
+        <div className="flex flex-wrap -mb-px text-base font-medium text-center text-gray-500 dark:text-gray-400">
+          <button
+            className={`mr-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg ${
+              selectedFilter === "all"
+                ? "text-blue-700 border-b-2 font-medium border-blue-700"
+                : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group text-gray-600"
+            } py-4 px-6 block hover:text-blue-500 focus:outline-none`}
+            onClick={() => handleFilterChange("all")}
+          >
+            All
+          </button>
+          <button
+            className={`mr-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg ${
+              selectedFilter === "Boar"
+                ? "text-blue-700 border-b-2 font-medium border-blue-700"
+                : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group text-gray-600"
+            } py-4 px-6 block hover:text-blue-500 focus:outline-none`}
+            onClick={() => handleFilterChange("Boar")}
+          >
+            Boar
+          </button>
+          <button
+            className={`mr-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg ${
+              selectedFilter === "Deer"
+                ? "text-blue-700 border-b-2 font-medium border-blue-700"
+                : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group text-gray-600"
+            } py-4 px-6 block hover:text-blue-500 focus:outline-none`}
+            onClick={() => handleFilterChange("Deer")}
+          >
+            Deer
+          </button>
+        </div>
+      </div>
       <div id="map" style={{ height: "400px" }}></div>
     </div>
   );
